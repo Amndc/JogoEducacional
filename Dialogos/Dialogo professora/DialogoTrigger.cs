79a5868689b5ac9a1cf4b4afc99c652de991dc22
Amanda Cardoso <61628738+Amndc@users.noreply.github.com>
2022-06-05 20:57:25 -0300

    Add files via upload

---
+++ Dialogos/Dialogo professora/DialogoTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogoTrigger : MonoBehaviour
+{
+    public Dialogo dialogo;
+
+    public void TriggerDialogo()
+    {
+        FindObjectOfType<GerencDialogo>().StartDialogo(dialogo);
+        FindObjectOfType<BalaoDeFala>().OnMouseDown();
+    }
+
+}
