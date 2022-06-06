8c18e301e0a8b1fb14e105e5a51c204c28780651
Amanda Cardoso <61628738+Amndc@users.noreply.github.com>
2022-06-05 21:17:42 -0300

    Scripts dialogo professora e textos lousa.
    Scripts de exibição dos diálogos da professora com o aluno e dos textos de explicação que devem ser exibidos na lousa para o aluno.

---
+++ ScriptsDialogos/BalaoDeFala.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BalaoDeFala : MonoBehaviour
+{
+  
+    private float distancia;
+
+    private GameObject player;
+
+    [SerializeField]
+    public float DistMin;
+
+    [SerializeField] GameObject Importante;
+
+    [SerializeField] GameObject Lousa;
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Balao();
+        
+    }
+
+    public void Balao()
+    {
+        distancia = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distancia <= DistMin)
+        {
+            Importante.SetActive(true);
+        }
+
+    }
+
+    public void OnMouseDown()
+    {
+
+        Destroy(Importante);          
+        Lousa.SetActive(true);
+            
+    }
+}
+
---
+++ ScriptsDialogos/Dialogo professora/GerencDialogo.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GerencDialogo : MonoBehaviour
+{
+    [SerializeField]
+    public TextMeshProUGUI nomeText;
+
+    [SerializeField]
+    public TextMeshProUGUI dialogoText;
+
+    public Animator anim;
+
+    private Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogo(Dialogo dialogo) 
+    {
+        anim.SetBool("IsOpen", true);
+        nomeText.text = dialogo.nameNPC;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogo.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();        
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogo();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogoText.text = sentence;
+    }
+   
+    void EndDialogo()
+    {
+        anim.SetBool("IsOpen", false);
+    }
+
+}
